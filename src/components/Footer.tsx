1da0ad071d91485b156d667dd5315987c8017cb1
heythemba <heythem.94@gmail.com>
2025-11-20 17:47:17 +0100

    feat: add Gmail compose link to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,10 +12,16 @@ export function Footer() {
           <p className="text-xl text-slate-300 max-w-2xl mx-auto">
             I'm always open to discussing new projects, creative ideas, or opportunities to be part of your vision.
           </p>
-            <Button size="lg" variant="secondary" className="gap-2">
-            <Mail className="w-5 h-5" />
-            haythem.bag@gmail.com
-          </Button>
+            <a
+              href="https://mail.google.com/mail/?view=cm&fs=1&to=haythem.bag@gmail.com&body=Hi%20Haythem%2C%0A%0AI%20hope%20this%20email%20finds%20you%20well%2C%20i%20would%20like%20to%20%5Breplace%20with%20your%20message%5D%0A%0ABest%20Regard%2C%0A%5Breplace%20with%20your%20name%5D"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Button size="lg" variant="secondary" className="gap-2">
+                <Mail className="w-5 h-5" />
+                haythem.bag@gmail.com
+              </Button>
+            </a>
         </div>
 
         <div className="border-t border-slate-800 pt-8">
